babfee34f8ec0f9139fa13b69edde7595b2cfb0a
Pooja R <90558364+PoojaShingri@users.noreply.github.com>
2024-03-02 15:26:11 +0530

    Source code Pharmacy Management

---
+++ PharmacyManagementSupplySystem/Frontend/src/app/common/Urls.ts
@@ -0,0 +1,8 @@
+export const SIGN_IN_URL = 'http://localhost:8081/auth/signin';
+export const SIGN_UP_URL = 'http://localhost:8081/auth/signup';
+export const VALIDATE_URL = 'http://localhost:8081/auth/validate';
+export const MEDICINE_STOCK_INFORMATION_URL = 'http://localhost:8082/medicine-stock/medicine/stock';
+export const REPRESENTATIVE_SCHEDULE_URL = 'http://localhost:8085/api/medical-representative-schedule-service/RepSchedule';
+export const PHARMACY_SUPPLY_URL = "http://localhost:8086/pharmacy-medicine-supply/pharmacy-supply";
+export const PHARMACY_SUPPLY_VIEW_DEMAND_URL = "http://localhost:8086/pharmacy-medicine-supply/viewDemand";
+export const PHARMACY_SUPPLY_DETAILS_URL = "http://localhost:8086/pharmacy-medicine-supply/pharmacy-medicine-supply";
